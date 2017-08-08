e3f80f845e53baea6175bc13eeda6aaa1bd9be40
Arika Chen <eaglesora@gmail.com>
2017-08-08 17:17:25 +0800

    add nginx service manage
    Signed-off-by: Arika Chen <eaglesora@gmail.com>

--- pkg/lbm/nginx.go
+++ pkg/lbm/nginx.go
@@ -2,12 +2,17 @@ package lbm
 
 import (
 	"encoding/json"
+	"fmt"
+	"os"
 	"path"
+
+	"github.com/ArikaChen/lbmanager/pkg/utils"
 
 	"github.com/golang/glog"
 )
 
 const (
+	cmd           = "/usr/sbin/nginx"
 	checkPattern  = "%s -t"
 	reloadPattern = "%s -s reload"
 	pidFile       = "/run/nginx.pid"
@@ -17,16 +22,14 @@ type NginxImpl struct {
 	LBBase
 
 	streamConfPath string
-	nginxCMD       string
-	local          bool
 }
 
 func NewNginxImpl() *NginxImpl {
 	lm := &NginxImpl{
-		LBBase:   NewLB(),
-		nginxCMD: "",
+		LBBase: NewLB(),
 	}
 	lm.ConfigPath = path.Join(lm.Conf.ConfDir, "conf.d")
+	lm.streamConfPath = path.Join(lm.Conf.ConfDir, "stream.conf.d")
 	lm.Elem = make(map[string]interface{})
 	lm.Impl = lm
 	return lm
@@ -49,30 +52,64 @@ func (n *NginxImpl) Parse(key string, data []byte) (interface{}, error) {
 	return lb, nil
 }
 
+func (n *NginxImpl) GetConfigFilePath(tp, name string) string {
+	filePath := n.ConfigPath
+	if tp != "HTTP" {
+		filePath = n.streamConfPath
+	}
+	return path.Join(filePath, name+".conf")
+}
+
 func (n *NginxImpl) ConfigService(lb interface{}) bool {
 	ng := lb.(*Nginx)
-	filePath := n.GetConfigFilePath(ng.Name)
+	filePath := n.GetConfigFilePath(ng.Protocol, ng.Name)
 	needRelod, _ := n.Render(filePath, nginxTmpl, lb)
 	return needRelod
 }
 
 func (n *NginxImpl) DeleteService(name string) bool {
+	prot := []string{"HTTP", "L4"}
+
+	for _, p := range prot {
+		filename := n.GetConfigFilePath(p, name)
+		if isExist, _ := utils.IsFileExist(filename); isExist {
+			glog.Infof("Delete lb file %v", filename)
+			if err := os.Remove(filename); err != nil {
+				glog.Warningf("Failed to delete %v: %v", filename, err)
+				return false
+			}
+			return true
+		}
+	}
 	return false
 }
 
 func (n *NginxImpl) Reload() {
-	/*
-		check := fmt.Sprintf(checkPattern, n.nginxCMD)
-		if err := utils.ShellOut(check); err != nil {
-			return fmt.Errorf("Invalid nginx configuration detected, not reloading: %s", err)
-		}
-		cmd := fmt.Sprintf(reloadPattern, n.nginxCMD)
-		if err := utils.ShellOut(cmd); err != nil {
-			return fmt.Errorf("Reloading NGINX failed: %s", err)
-		}*/
-	glog.V(3).Info("Reloading nginx")
+	//TODO rate limit
+	check := fmt.Sprintf(checkPattern, cmd)
+	if err := utils.ShellOut(check); err != nil {
+		glog.Warningf("Invalid nginx configuration detected, error: %s", err)
+		return
+	}
+	cmd := fmt.Sprintf(reloadPattern, cmd)
+	if err := utils.ShellOut(cmd); err != nil {
+		glog.Warningf("Reloading nginx failed, error: %s", err)
+		return
+	}
+	glog.Infof("Reloading nginx")
 }
 
 func (n *NginxImpl) Recover() error {
 	return nil
+}
+
+func (n *NginxImpl) Init() {
+	if utils.CheckThreadExist(pidFile, cmd) {
+		glog.Infof("nginx is already started")
+		return
+	}
+
+	if err := utils.ShellOut(cmd); err != nil {
+		glog.Fatalf("Failed to start nginx")
+	}
 }
